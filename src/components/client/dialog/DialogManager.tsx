47e8e27dce0f0a1dc636356db23d8bef2a57120c
ralph <ralph@localhost>
2022-11-21 17:00:39 +0100

    upgrade dialog api and introduce verification alert

--- src/components/client/dialog/DialogManager.tsx
+++ src/components/client/dialog/DialogManager.tsx
@@ -4,16 +4,74 @@ import Dialog from './Dialog';
 import DialogButton from './DialogButton';
 
 /**
+ * Dialog manager state.
+ */
+interface DialogMgrState {
+    /**
+     * The dialogs to display.
+     */
+    dialogs: React.ReactElement<Dialog>[];
+}
+
+/**
  * Dialog window manager.
  */
-class DialogManager extends React.Component<{}> {
+class DialogManager extends React.Component<{}, DialogMgrState> {
     constructor(props: {}) {
         super(props);
+
+        this.state = {
+            dialogs: []
+        }
+    }
+
+    /**
+     * Shows the specified dialog.
+     * @param dlg The dialog to show.
+     */
+    showDialog(dlg: React.ReactElement<Dialog>) {
+        this.state.dialogs.push(dlg);
+
+        this.setState({
+            dialogs: this.state.dialogs 
+        });
+    }
+
+    /**
+     * Creates a simple alert.
+     * @param title The title of the alert.
+     * @param message The message to show.
+     */
+    createAlert(title: string, message: string) {
+        let dlgKey = "alert" + this.state.dialogs.length;
+
+        return new Promise((resolve) => {
+            this.showDialog(<Dialog title={title} content={message} key={dlgKey} buttons={[
+                <DialogButton key="ok" label="OK" onclick={()=>{
+                    resolve(void 0);
+                    this.closeDialog(dlgKey);
+                }}></DialogButton>
+            ]}></Dialog>);
+        });
+    }
+
+    /**
+     * Closes the specified dialog.
+     * @param id The ID (key) of the dialog to close.
+     */
+    closeDialog(id: string) {
+        const dlgIdx = this.state.dialogs.findIndex(x => x.key == id);
+
+        if(dlgIdx !== -1)
+            this.state.dialogs.splice(dlgIdx, 1);
+
+        // Reload state
+        this.setState(this.state);
     }
 
     render() {
         return <div className='dialog-container'>
-            
+            {this.state.dialogs}
         </div>
     }
 }
--- src/pages/client/Client.tsx
+++ src/pages/client/Client.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import UserEntity from '../../api/entities/UserEntity';
+import Dialog from '../../components/client/dialog/Dialog';
 import DialogManager from '../../components/client/dialog/DialogManager';
 import GuildBar from '../../components/client/layout/GuildBar';
 import UserPaneContainer from '../../components/client/layout/UserPaneContainer';
@@ -11,9 +12,37 @@ interface ClientProps {
     user: UserEntity;
 }
 
-class ClientPage extends React.Component<ClientProps> {
+/**
+ * Represents the current client state.
+ */
+interface ClientState {
+    user: UserEntity;
+}
+
+class ClientPage extends React.Component<ClientProps, ClientState> {
+    /**
+     * Dialog management reference.
+     */
+    private dlgMgrRef = React.createRef<DialogManager>();
+
     constructor(props: ClientProps) {
         super(props);
+
+        this.state = {
+            user: props.user
+        };
+
+        console.log(this.state.user);
+    }
+
+    /**
+     * Client init function.
+     */
+    componentDidMount(): void {
+        // Check if user has a pending verification
+        if(this.state.user.verifStatus == "AWAIT_VERIF")
+            // Show dialog asking for email check
+            this.dlgMgrRef.current.createAlert("Account Verification Needed", "Please check your email for a confirmation link. You will need to verify your account to continue using DiscDoor.");
     }
 
     render() {
@@ -24,7 +53,7 @@ class ClientPage extends React.Component<ClientProps> {
             - content pane
         */
         return <div className='client'>
-            <DialogManager></DialogManager>
+            <DialogManager ref={this.dlgMgrRef}></DialogManager>
             <GuildBar></GuildBar>
             <UserPaneContainer user={this.props.user}></UserPaneContainer>
         </div>
