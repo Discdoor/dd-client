46d5de7c8fed4ba008f46c3113929e3e3ddd3d38
ralph <ralph@localhost>
2022-11-21 17:14:05 +0100

    add heartbeat check to ensure user is still logged in

--- src/pages/client/Client.tsx
+++ src/pages/client/Client.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
+import { performAPIRequest } from '../../api/APIFetch';
+import { getAPIDefinitions } from '../../api/APIProps';
+import { APIResponse } from '../../api/APIResponse';
 import UserEntity from '../../api/entities/UserEntity';
-import Dialog from '../../components/client/dialog/Dialog';
 import DialogManager from '../../components/client/dialog/DialogManager';
 import GuildBar from '../../components/client/layout/GuildBar';
 import UserPaneContainer from '../../components/client/layout/UserPaneContainer';
+const clientCfg = require('../../../config/client-cfg.json');
 
 /**
  * Client properties.
@@ -36,6 +39,19 @@ class ClientPage extends React.Component<ClientProps, ClientState> {
     }
 
     /**
+     * Heartbeat check - ensures we are still logged in etc.
+     */
+    async performHeartbeat() {
+        const endpoint = getAPIDefinitions().gwServer + "/auth/session/validate";
+        const resp = await performAPIRequest<APIResponse<null>>(endpoint, "POST", {});
+        
+        if((!resp.success) && (resp.code == 401)) {
+            // User is not signed in anymore, redir to login page
+            window.location.href = "/login";
+        }
+    }
+
+    /**
      * Client init function.
      */
     componentDidMount(): void {
@@ -43,6 +59,9 @@ class ClientPage extends React.Component<ClientProps, ClientState> {
         if(this.state.user.verifStatus == "AWAIT_VERIF")
             // Show dialog asking for email check
             this.dlgMgrRef.current.createAlert("Account Verification Needed", "Please check your email for a confirmation link. You will need to verify your account to continue using DiscDoor.");
+    
+        // Start heartbeat routine
+        setInterval(()=>this.performHeartbeat(), clientCfg.HB_REFRESH_RATE);
     }
 
     render() {
